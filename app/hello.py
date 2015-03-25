d3967bd97fa81b65a26af3bdac0220a11bd29cd6
Adam Burvill <adam.burvill@osedea.com>
2015-03-24 21:37:33 -0400

    Created otp class and added static and templates

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
+import otp
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+	return render_template('home.html');
+    # return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ app/otp.py
@@ -0,0 +1,2 @@
+def test():
+	return 'Here';
